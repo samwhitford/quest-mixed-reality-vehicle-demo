243e23c5693def1542feffa05ca85de734c920e8
samwhitford <samwhitford@gmail.com>
2025-10-29 15:55:21 +1100

    ramp: better scaling

--- src/App/World/ramp.js
+++ src/App/World/ramp.js
@@ -20,17 +20,18 @@ export default class Ramp {
       this.debug = state.debug;
     });
 
-    this.config = {
-      scaleFactor: 0.2,
+    this.options = {
+      scaleFactor: 1,
       quantity: 1,
       position: new THREE.Vector3(-0.2, 1, -3), // spawn point
       rotation: new THREE.Quaternion().setFromAxisAngle(
         new THREE.Vector3(0, 1, 0), // Y-axis
         Math.PI                      // 180 degrees
-      )
+      ),
+      ...options
     };
     this.rampGroup = new THREE.Group();
-    this.scale = new THREE.Vector3().setScalar(this.config.scaleFactor)
+    this.scale = new THREE.Vector3().setScalar(this.options.scaleFactor)
     this.mesh.scale.copy(this.scale)
     this.mesh.updateMatrixWorld(true);
     this.mesh.traverse(function(child){
@@ -38,18 +39,20 @@ export default class Ramp {
     })
     let meshClone = this.mesh.clone();
     console.log(meshClone)
-    meshClone.position.copy(this.config.position);
-    meshClone.applyQuaternion(this.config.rotation);
+    meshClone.position.copy(this.options.position);
+    meshClone.applyQuaternion(this.options.rotation);
     this.rampGroup.add(meshClone);
     this.confetti = new ConfettiCannon({
       particleCount: 500,
-      cylinderRadius: 0.05,
-      cylinderHeight: 0.2,
+      cylinderRadius: 0.5 * this.options.scaleFactor,
+      cylinderHeight: 0.2 * this.options.scaleFactor,
       initialSpeed: 15,
       spreadAngle: 30, // Vertical spread
       gravity: -20,
       drag: 0.5,
       duration: 0.25,
+      // offsetFromParent: new THREE.Vector3(0,0.16,0.25),
+      offsetFromParent: new THREE.Vector3(0,1,1.1).multiplyScalar(this.options.scaleFactor),
     });
     for (const child of meshClone.children) {
       child.traverse((obj) => {
@@ -59,9 +62,9 @@ export default class Ramp {
           this.meshArray.push(obj);
           this.physics.add(obj, "dynamic", "convexHull");
           this.physics.meshMap.get(obj).setSoftCcdPrediction(2);
-          obj.userData.originalPos = this.config.position;
+          obj.userData.originalPos = this.options.position;
           obj.userData.originalPos.setX(-0.05);
-          obj.userData.originalRot = this.config.rotation;
+          obj.userData.originalRot = this.options.rotation;
           obj.material.side = THREE.FrontSide;
           this.confetti.options.parentMesh = this.physics.meshMap.get(obj);
         }

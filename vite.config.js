0b670c1c7356154d0491846bcf75394327bbbaa8
samwhitford <samwhitford@gmail.com>
2025-10-29 20:36:16 +1100

    tweak vite config

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ import topLevelAwait from "vite-plugin-top-level-await";
 export default {
   server: {
     host: '0.0.0.0',
+    allowedHosts: true,
   },
   root: "src/",
   publicDir: "../static/",
-  base: "./",
+  base: './',
   plugins: [wasm(), topLevelAwait()],
   build: {
     rollupOptions: {
